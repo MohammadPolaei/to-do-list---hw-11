d91b6aa96296255f614d068282327614ed61c7e0
Mohammad Polaei <mohammadpolaei@gmail.com>
2025-11-04 08:34:38 +0330

    fix: dataOfLocalstorage.js updated and works in LocalStorage

--- src/utils/EL/dataOfLocalstorage/dataOfLocalstorage.js
+++ src/utils/EL/dataOfLocalstorage/dataOfLocalstorage.js
@@ -2,11 +2,43 @@ import { InputDiv } from "../../userInput/userInput";
 
 export function ToAddIntoLocalstorage() {
 	const theInputs = InputDiv();
-
 	const submitButton = theInputs.querySelector("#userInputButton");
+	// datas
 
 	submitButton.addEventListener("click", () => {
-		console.log("clicked");
+		// title
+		const titleOfInput = theInputs.querySelector("#userInputForTodo");
+		// priority
+		const priorities = theInputs.getElementsByClassName("radio");
+		const resultOfPriority = Array.from(priorities).filter(
+			(item) => item.checked
+		)[0];
+		// status
+		const status = theInputs.getElementsByClassName("status");
+		const resultOfStatus = Array.from(status).filter((item) => item.checked)[0];
+		// date
+		const inputDate = theInputs.getElementsByClassName("inputDate")[0];
+
+		// sending data into LOCALSTORAGE
+		if (
+			titleOfInput == undefined ||
+			titleOfInput.value == "" ||
+			resultOfPriority == undefined ||
+			resultOfStatus == undefined ||
+			inputDate.value == ""
+		) {
+			console.log("Inputs must be filled !");
+		} else {
+			localStorage.setItem(
+				`list ${titleOfInput.value}`,
+				JSON.stringify({
+					title: `${titleOfInput.value}`,
+					priority: `${resultOfPriority.attributes.id.value}`,
+					status: `${resultOfStatus.attributes.id.value}`,
+					date: `${inputDate.value}`,
+				})
+			);
+		}
 	});
 	return theInputs;
 }
--- src/utils/userInput/userInput.js
+++ src/utils/userInput/userInput.js
@@ -1,39 +1,41 @@
 import { El } from "../EL/EL";
 
 export function InputDiv() {
+	// input box
+
+	const inputsBox = El({
+		element: "div",
+		classList:
+			"w-[50%] bg-[#00000088] backdrop-blur-xl p-10 shadow-2xl flex flex-col gap-3 items-center rounded-xl",
+	});
+	// input container
 	const inputsContainer = El({
 		element: "div",
-		classList: "flex items-center justify-center static",
-		children: [
-			El({
-				element: "div",
-				classList:
-					"w-[50%] bg-[#00000088] p-10 shadow-2xl flex flex-col gap-3 items-center rounded-xl",
-			}),
-		],
+		classList: "flex items-center justify-center absolute w-full top-5",
+		children: [inputsBox],
 	});
 	inputsContainer.children[0].innerHTML = `<input id="userInputForTodo" type="text" placeholder="Task Title" class="rounded-xl bg-[#ffffff99] border-1 px-2 outline-0"> 
   <div class="flex flex-row gap-2">
-    <p>Priority</p>
+    <p class="font-bold">Priority : </p>
     <label for="low">low</label>
-    <input id="low" name="priority" type="radio">
+    <input id="low" class="radio" name="priority" type="radio">
     <label for="medium">medium</label>
-    <input id="medium" name="priority" type="radio">
+    <input id="medium" class="radio" name="priority" type="radio">
     <label for="high">high</label>
-    <input id="high" name="priority" type="radio">
+    <input id="high" class="radio" name="priority" type="radio">
   </div>
   <div class="flex flex-row gap-2">
-    <p>Status</p>
+    <p class="font-bold">Status : </p>
     <label for="todo">todo</label>
-    <input id="todo" name="status" type="radio">
+    <input class="status" id="todo" name="status" type="radio">
     <label for="doing">doing</label>
-    <input id="doing" name="status" type="radio">
+    <input class="status" id="doing" name="status" type="radio">
     <label for="done">done</label>
-    <input id="done" name="status" type="radio">
+    <input class="status" id="done" name="status" type="radio">
   </div>
   <div class="flex flex-row gap-2">
-    <p>Deadline</p>
-    <input type="date">
+    <p class="font-bold">Deadline : </p>
+    <input class="inputDate" type="date">
   </div>
   <button id="userInputButton" class=" bg-white rounded-2xl p-2">submit</button>
   `;
